8a9252253b2df6985b9a2aca0a049ba0a215fd0e
hanmannverde686 <hanmannverde686@gmail.com>
2025-03-10 09:05:47 +0000

    Add 5.php

---
+++ src/5.php
@@ -0,0 +1,20 @@
+<?php
+$number = rand(1, 5); // random number between 1 and 5
+switch ($number) {
+case 1:
+echo "The answer is 42";
+break;
+case 2:
+echo "The answer is 17";
+break;
+case 3:
+echo "The answer is 99";
+break;
+case 4:
+echo "The answer is 16";
+break;
+case 5:
+echo "The answer is 41";
+break;
+}
+?>
