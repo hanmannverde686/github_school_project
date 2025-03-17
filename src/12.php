8a2f001b8654d71bd924ebba2611e237790d4ddc
hanmannverde686 <hanmannverde686@gmail.com>
2025-03-17 06:18:00 +0000

    Add 12.php

---
+++ src/12.php
@@ -0,0 +1,17 @@
+  <?php
+  
+// Define variables
+$name = 'John Doe';
+$age = 30;
+$gender = 'male';
+$interests = array('reading', 'writing', 'hiking');
+
+// Print output
+echo $name . '<br>';
+echo $age . '<br>';
+echo $gender . '<br>';
+echo $interests[0] . '<br>';
+echo $interests[1] . '<br>';
+echo $interests[2] . '<br>';
+
+?>
