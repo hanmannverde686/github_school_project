e484ca6d75cad29ad70346bfc43d7737e0e5f426
hanmannverde686 <hanmannverde686@gmail.com>
2025-03-22 16:34:34 +0000

    Add 16.php

---
+++ src/16.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "school_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query
+$sql = "SELECT * FROM students WHERE id = 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // Output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+$conn->close();
+?>
