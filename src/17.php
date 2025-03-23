6cb7760ceea407950484b3153d9d387847cea7a1
hanmannverde686 <hanmannverde686@gmail.com>
2025-03-23 10:13:46 +0000

    Add 17.php

---
+++ src/17.php
@@ -0,0 +1,8 @@
+<?php
+// This is an example PHP code for a simple school project.
+// It demonstrates basic PHP functionality such as outputting messages and variables.
+// You can extend this code to include more complex logic or features depending on your requirements.
+
+echo "Hello, World!";
+echo "This is the second line of code.";
+?>
