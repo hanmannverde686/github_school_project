8d315abf390e702348c54cec977e75b449104e7b
hanmannverde686 <hanmannverde686@gmail.com>
2025-04-05 05:06:14 +0000

    Add 24.php

---
+++ src/24.php
@@ -0,0 +1,16 @@
+<?php
+// Define some variables
+$a = 5;
+$b = 10;
+
+// Perform some calculations or operations on a and b
+$sum = $a + $b;
+$difference = $a - $b;
+$product = $a * $b;
+
+// Output the results
+echo "The sum of $a and $b is: $sum\n";
+echo "The difference between $a and $b is: $difference\n";
+echo "The product of $a and $b is: $product\n";
+
+?>
